daad46f2aa5871d8f55e73f53bf5670063c59d8f
Jesper B. Rosenkilde <jbr@humppa.dk>
2021-01-31 21:43:30 +0100

    Only run and update when packages are missing

--- include/packages.mk
+++ include/packages.mk
@@ -1,25 +1,37 @@
 ##
-# Set packages-want to a list of packages in a module
-# to have those packages automagically installed
-#
-# eg:
-# packages-want := git unzip
+# Usage:
+# packages-want = git unzip
+# include packages.mk
+# target: packages-insall
+
+.SECONDEXPANSION:
 
 .PHONY: nop
 nop:
 
 export packages_import_count += 1
-
-.PHONY: packages-install
-packages-install: have = $(shell dpkg-query -W -f '$${Package}\n')
-packages-install: missing = $(filter-out $(have),$(packages-want))
-packages-install: ${trunk}/.packages-cache-update
-	@sudo apt-get install -y $(missing)
 ifeq ($(packages_import_count), 1)
-	@rm -f "$<"
+_packages_clean_up = .packages_clean_up
 endif
+
+.PHONY: .packages_clean_up
+.packages_clean_up:
+	@rm -f "${trunk}/.packages-cache-update"
 
 ${trunk}/.packages-cache-update: 
 	@sudo apt-get update
 	@touch $@
+
+_packages_have = $(shell dpkg-query -W -f '$${Package}\n')
+_packages_missing = $(filter-out $(_packages_have),$(packages-want))
+ifneq ($(_packages_missing),)
+_packages_apt-install = .packages_apt-install
+endif
+
+.PHONY: .packages_apt-install
+.packages_apt-install: $(trunk)/.packages-cache-update
+	@sudo apt-get install -y $(_packages_missing)
+
+.PHONY: packages-install
+packages-install: $(_packages_apt-install) $(_packages_clean_up)
 
